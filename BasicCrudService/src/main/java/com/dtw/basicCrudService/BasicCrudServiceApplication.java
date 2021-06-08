4af7d75578f6f8a350611761e28630d0134f7c83
David de Castro Hernandez <itsdavidch@gmail.com>
2021-06-08 05:49:18 +0200

    Initial commit of BasicCrudService

---
+++ BasicCrudService/src/main/java/com/dtw/basicCrudService/BasicCrudServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dtw.basicCrudService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCrudServiceApplication.class, args);
+	}
+
+}
