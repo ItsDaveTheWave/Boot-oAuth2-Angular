74bfb799c4bc9a20d16c35595b5d63c142333b14
David de Castro Hernandez <itsdavidch@gmail.com>
2021-06-09 06:06:12 +0200

    Created service operations

--- AngularWebClient/src/app/service/car.service.ts
+++ AngularWebClient/src/app/service/car.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Car } from '../model/car';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor() { }
+  url: string = 'http://localhost:8080/car/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAll(): Observable<Car[]> {
+    return this.httpClient.get<Car[]>(this.url);
+  }
+
+  public getOne(id: number): Observable<Car> {
+    return this.httpClient.get<Car>(this.url + id);
+  }
+
+  public save(car: Car): Observable<any> {
+    return this.httpClient.post<any>(this.url, car);
+  }
 }
